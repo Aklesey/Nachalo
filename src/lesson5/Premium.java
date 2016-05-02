2e47162ac42e8a7fe4dc9d71ef20654b4a8ef124
Aklesey <alex.sheptura@mail.ru>
2016-05-02 10:50:09 +0300

    will be finished homework lesson 5

--- src/lesson5/Premium.java
+++ src/lesson5/Premium.java
@@ -9,14 +9,46 @@ public class Premium {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите свою ставку");
-        float money = input.nextFloat();
+        double money = input.nextDouble(), premium;
         System.out.println("Введите количество отработаных лет");
         int years = input.nextInt();
-        switch (years) {
-            case (years < 5):
+        if (years >= 0 && years < 5) {
+            premium = (money * 1.1);
+            premium = premium - money;
+            System.out.println("Премиальная надбавка состовляет 10 % от вашей текущей заработной платы: " + premium);
+        } else {
+            if (years >= 5 && years < 10) {
+                premium = (money * 1.15);
+                premium = premium - money;
+                System.out.println("Премиальная надбавка состовляет 15 % от вашей текущей заработной платы: " + premium);
+            } else {
+                if (years >= 10 && years < 15) {
+                    premium = (money * 1.25);
+                    premium = premium - money;
+                    System.out.println("Премиальная надбавка состовляет 25 % от вашей текущей заработной платы: " + premium);
+                } else {
+                    if (years >= 15 && years < 20) {
+                        premium = (money * 1.35);
+                        premium = premium - money;
+                        System.out.println("Премиальная надбавка состовляет 35 % от вашей текущей заработной платы: " + premium);
+                    } else {
+                        if (years >= 20 && years < 25) {
+                            premium = (money * 1.45);
+                            premium = premium - money;
+                            System.out.println("Премиальная надбавка состовляет 45 % от вашей текущей заработной платы: " + premium);
+                        } else {
+                            if (years >= 25) {
+                                premium = (money * 1.5);
+                                premium = premium - money;
+                                System.out.println("Премиальная надбавка состовляет 50 % от вашей текущей заработной платы: " + premium);
+                            } else {
+                                System.out.println("Вы значит не работаете в нашей компании =(");
+                            }
 
-
+                        }
+                    }
+                }
+            }
         }
-
     }
 }
